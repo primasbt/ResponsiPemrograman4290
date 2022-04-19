fc78f9f57e3839a47768f27c0577d128b2df675e
Primasbt <91203079+primasbt@users.noreply.github.com>
2022-04-19 17:52:27 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace ResponsiPemrograman4290
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Karyawan karyawan1 = new Karyawan("190302123", "Paijo", 4000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "Jono", 2000000);
+
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.tampilkanData();
+            Console.Write("2 "); karyawan2.tampilkanData();
+            Console.WriteLine();
+
+            karyawan1.naikGajiKaryawan();
+            karyawan2.naikGajiKaryawan();
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama      Gaji Bulanan");
+            Console.WriteLine("-----------------------------");
+            Console.Write("1 "); karyawan1.tampilkanData();
+            Console.Write("2 "); karyawan2.tampilkanData();
+
+            Console.WriteLine();
+            Console.WriteLine("tekan sembarang key untuk menyudahi....");
+            Console.ReadKey();
         }
     }
 }
